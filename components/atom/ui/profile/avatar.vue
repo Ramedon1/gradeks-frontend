69f2d98c6b09e07e6f79b17481c34488510997f7
Ramedon <info@ramed.one>
2024-11-17 15:41:26 +0500

    ✨ Update loading avatar cached

--- components/atom/ui/profile/avatar.vue
+++ components/atom/ui/profile/avatar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import {useAuthStore} from "~/state/auth";
+import { useAuthStore } from "~/state/auth";
+const { userInfo } = storeToRefs(useAuthStore());
 
 const props_avatar = defineProps({
   size: {
@@ -8,13 +9,20 @@ const props_avatar = defineProps({
   }
 });
 
-const {userInfo} = storeToRefs(useAuthStore());
-const url = `https://api.gradeks.xyz/user/avatar/${userInfo.value.id}`;
+const avatarKey = `avatar_${userInfo.value.id}`;
+let cachedAvatar = localStorage.getItem(avatarKey);
+let timestamp = Date.now();
+let url = `https://api.gradeks.xyz/user/avatar/${userInfo.value.id}`;
+
+if (!cachedAvatar || !cachedAvatar.includes(userInfo.value.id)) {
+  cachedAvatar = `${url}?t=${timestamp}`;
+  localStorage.setItem(avatarKey, cachedAvatar);
+}
+
 </script>
 
-
 <template>
-  <img :src="url"
+  <img :src="cachedAvatar"
        alt="avatar" class="avatar"
        :style="{ width: props_avatar.size + 'px', height: props_avatar.size + 'px' }">
 </template>
