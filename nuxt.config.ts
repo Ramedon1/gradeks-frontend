cf224946f11084434710922aed6e5ebd1705690b
Ramedon <info@ramed.one>
2024-11-23 18:53:13 +0500

    ➖ Disable async tg script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
             script: [
                 {
                     src: "https://telegram.org/js/telegram-web-app.js",
-                    async: true,
                 },
             ],
         },
