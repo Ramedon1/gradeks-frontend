a55747e9721b2f71a637aa410cca610073f2d064
Ramedon <info@ramed.one>
2025-04-20 19:14:16 +0500

    ⚡️ tag priority to Telegram WebApp script for improved loading behavior

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
             script: [
                 {
                     src: "https://telegram.org/js/telegram-web-app.js",
+                    tagPriority: 0
                 },
             ],
         },
