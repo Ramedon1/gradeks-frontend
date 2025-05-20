28a175e8e2712c52b9d878cec74cd32533cea3d2
Ramedon <info@ramed.one>
2025-05-21 03:33:36 +0500

    🐛 Fix error handling in Telegram auth middleware for unknown environment scenarios

--- middleware/telegram-auth-middleware.global.ts
+++ middleware/telegram-auth-middleware.global.ts
@@ -9,7 +9,6 @@ import { useAuthStore, type UserLoginInterface } from "../state/auth";
 import { useDiaryState } from "../state/diary";
 import { getSettings, initializeDefaults } from "~/composables/useLocalStorage";
 import { applyTheme } from "assets/js/functions";
-import type { H3Error } from "h3";
 
 const initDataRawMocked = "user=%7B%22id%22%3A646667177%2C%22first_name%22%3A%22Ramedon%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22ramedon%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FjyMbRbhD4Sby1w6rVXT7N-ShSD36qph9lAogP4Tjev8.svg%22%7D&chat_instance=1872905771921478843&chat_type=sender&auth_date=1734440065&signature=07-Re0J_FAtgQ_a9CnpCDkfPihbPvsxrMNfAPhmTZTf96w7HbTM333unzkIu_ZSHF9fy35tXc6dfh0An42p8Bg&hash=4679ace8a3d67cdf75920909b65930e294970614f7c919183a4c74b9ec1064d7";
 
@@ -49,8 +48,9 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
         platform    = params.platform;
         version     = params.version;
     } catch (err: unknown) {
-        const e = err as H3Error;
-        if (e.message === "ERR_UNKNOWN_ENV") {
+        const e = err as any;
+        if (e.code === "ERR_UNKNOWN_ENV" ||
+            (typeof e.message === "string" && e.message.includes("Unable to retrieve launch parameters"))) {
             return navigateTo("https://t.me/gradeksbot", { external: true });
         }
         throw err;
@@ -61,7 +61,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     }
 
     themeParams.mount();
-
 
     const { authenticateUser } = useAuthStore();
     const { authenticated, error, loginData } = storeToRefs(useAuthStore());
@@ -74,11 +73,9 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
             platform,
             version,
         });
-
         if (error.value.detail) {
             return navigateTo("/unauthorized");
         }
-
         access_token.value = loginData.value?.access_token || null;
         initializeDefaults();
         applyTheme(getSettings("theme"));
